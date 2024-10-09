31d3d3d1e2066ea3b925aea1d5b2530baeede06c
asutosh <asutoshbiswal658@gmail.com>
2024-10-09 20:41:48 +0530

    Stage1.3 completed(LoginDAO/LoginSrevlet/LoginSucccessJSP)

--- User_App/src/main/java/Test_UserApp/LoginServlet.java
+++ User_App/src/main/java/Test_UserApp/LoginServlet.java
@@ -1,5 +1,25 @@
 package Test_UserApp;
 
-public class LoginServlet {
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
+@SuppressWarnings("serial")
+@WebServlet("/log")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        UserBean ub = new LoginDAO().login(req.getParameter("uname"), req.getParameter("pword"));
+        if (ub == null) {
+            req.setAttribute("msg", "Invalid Login Process...<br>");
+            req.getRequestDispatcher("Msg.jsp").forward(req, res);
+        } else {
+            ServletContext sct = req.getServletContext(); // Accessing Context Object reference
+            sct.setAttribute("ubean", ub); // UserBean object reference added to Context
+            Cookie ck = new Cookie("fName", ub.getfName()); // Creating Cookie
+            res.addCookie(ck); // Adding Cookie to response
+            // AddCookie method internally performs Serialization process
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, res);
+        }
+    }
 }
--- User_App/src/main/webapp/WEB-INF/LoginSuccess.jsp
+++ User_App/src/main/webapp/WEB-INF/LoginSuccess.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import ="Test_UserApp.UserBean"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,13 @@
 <title>Insert title here</title>
 </head>
 <body>
+<%
+UserBean ub = (UserBean)application.getAttribute("ubean");
+out.println("Welcome User : " + ub.getfName() + "<br>");
+%>
+<a href="view">ViewProfile</a>
+<a href="logout">Logout</a>
+
 
 </body>
 </html>
