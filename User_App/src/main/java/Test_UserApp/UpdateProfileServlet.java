1a439fe13ca1608f54b5a49ae77c7177e09de7af
asutosh <asutoshbiswal658@gmail.com>
2024-10-09 21:03:57 +0530

    Stage1.5Completed(EditProf.jsp/UpdateProfileDAO/UpdateProfileServ)

--- User_App/src/main/java/Test_UserApp/UpdateProfileServlet.java
+++ User_App/src/main/java/Test_UserApp/UpdateProfileServlet.java
@@ -1,5 +1,31 @@
 package Test_UserApp;
 
-public class UpdateProfileServlet {
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
+@SuppressWarnings("serial")
+@WebServlet("/update")
+public class UpdateProfileServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Cookie c[] = req.getCookies();
+        if (c == null) {
+            req.setAttribute("msg", "Session Expired...<br>");
+            req.getRequestDispatcher("Msg.jsp").forward(req, res);
+        } else {
+            String fName = c[0].getValue();
+            req.setAttribute("name", fName);
+            ServletContext sct = req.getServletContext();
+            UserBean ub = (UserBean) sct.getAttribute("ubean");
+            ub.setAddr(req.getParameter("addr"));
+            ub.setmId(req.getParameter("mid"));
+            ub.setPhNo(Long.parseLong(req.getParameter("phno")));
+            int k = new UpdateProfileDAO().update(ub);
+            if (k > 0) {
+                req.setAttribute("msg", "Profile Updated Successfully...<br>");
+                req.getRequestDispatcher("UpdateProfile.jsp").forward(req, res);
+            }
+        }
+    }
 }
--- User_App/src/main/webapp/WEB-INF/EditProfile.jsp
+++ User_App/src/main/webapp/WEB-INF/EditProfile.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import="Test_UserApp.UserBean"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,17 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+      <%
+String fName = (String)request.getAttribute("name");
+UserBean ub = (UserBean)application.getAttribute("ubean");
+out.println("Page belongs to : " + fName + "<br>");
+%>
+<form action="update" method="post">
+    Address:<input type="text" name="addr" value="<%=ub.getAddr() %>"><br>
+    MailId:<input type="text" name="mid" value="<%=ub.getmId() %>"><br>
+    PhoneNo:<input type="text" name="phno" value="<%=ub.getPhNo() %>"><br>
+    <input type="submit" value="UpdateProfile">
+</form>
+      
 </body>
 </html>
