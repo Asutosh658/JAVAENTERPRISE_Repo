b37d43b8fca607411a6d7166e83c62befd75e1a8
asutosh <asutoshbiswal658@gmail.com>
2024-10-09 21:07:31 +0530

    Stage1.6Completed(Updateprofile.jsp/LogoutServlet)

--- User_App/src/main/java/Test_UserApp/LogoutServlet.java
+++ User_App/src/main/java/Test_UserApp/LogoutServlet.java
@@ -1,5 +1,26 @@
 package Test_UserApp;
 
-public class LogoutServlet {
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
+@SuppressWarnings("serial")
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Cookie c[] = req.getCookies();
+        if (c == null) {
+            req.setAttribute("msg", "Session Expired...");
+        } else {
+            ServletContext sct = req.getServletContext();
+            sct.removeAttribute("ubean");
+            c[0].setMaxAge(0);
+            res.addCookie(c[0]);
+            req.setAttribute("msg", "User Logged out Successfully....");
+        }
+        req.getRequestDispatcher("Msg.jsp").forward(req, res);
+    }
 }
+
--- User_App/src/main/webapp/WEB-INF/UpdateProfile.jsp
+++ User_App/src/main/webapp/WEB-INF/UpdateProfile.jsp
@@ -8,5 +8,15 @@
 </head>
 <body>
 
+<%
+String fName = (String)request.getAttribute("name");
+String msg = (String)request.getAttribute("msg");
+out.println("Page belongs to : " + fName + "<br>");
+out.println(msg);
+%>
+<a href="view">ViewProfile</a>
+<a href="logout">Logout</a>
+
+
 </body>
 </html>
